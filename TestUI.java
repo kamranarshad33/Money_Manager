b0c359211402bb3581f0a9b10ff445a73b1f602a
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-11-21 17:31:38 -0500

    WIP

--- TestUI.java
+++ TestUI.java
@@ -1,3 +1,31 @@
-public class TestUI {
+import javax.swing.JOptionPane;
 
+public class TestUI {
+   
+   public static void main(String[] args) {
+      String start = "Welcome to the Money Manager! \n\n Do you have an existing account?";
+      
+      // Different methods based on existing or new account path?
+      if (JOptionPane.showConfirmDialog(null, start) == JOptionPane.YES_OPTION) {
+         existAcct();
+      }
+      else {
+         newAcct();
+      }
+
+   }
+   
+   // Existing Account path
+   public static void existAcct() {
+      System.out.println("Exisiting");
+      // Gather User Info
+   }
+   
+   // New Account path
+   public static void newAcct() {
+      System.out.println("New");
+      if (JOptionPane.showConfirmDialog(null, "Create an account?") == JOptionPane.YES_OPTION) {
+         System.out.println("createAcct()");
+      }   
+   }
 }
