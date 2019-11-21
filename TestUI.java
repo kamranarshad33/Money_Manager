a2235ef1dabe6d4e7d6bdca9a1ecded611082bbe
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-11-21 18:25:03 -0500

    update (1.0.2): Exisiting User path

--- TestUI.java
+++ TestUI.java
@@ -16,16 +16,36 @@ public class TestUI {
    }
    
    // Existing Account path
-   public static void existAcct() {
-      System.out.println("Exisiting");
+   public static void existAcct() { // validate + return validated Acct obj to main?
       // Gather User Info
+      String uname = "";
+      String pw = "";
+      boolean valid = false;
+      do {
+         uname = JOptionPane.showInputDialog(null, "Enter username: " + "hello", "Existing User", JOptionPane.QUESTION_MESSAGE);
+         pw = JOptionPane.showInputDialog(null, "Enter password: ", "Existing User", JOptionPane.QUESTION_MESSAGE);
+         /*
+            // Bank = placeholder for correct object
+            if (Bank.validateUser(uname, pw) {
+               valid = true;
+               JOptionPane.showMessageDialog(null, "Welcome back, " + uname + "!");
+            }
+            else {
+               if (JOptionPane.showConfirmDialog(null, "Would you like to try again? ", "Existing User", YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                  existAcct();
+               }
+            }
+         */
+         valid = true;
+      } while(!valid);
+      System.out.println("Existing");
    }
    
    // New Account path
    public static void newAcct() {
-      System.out.println("New");
       if (JOptionPane.showConfirmDialog(null, "Create an account?") == JOptionPane.YES_OPTION) {
          System.out.println("createAcct()");
-      }   
+      }
+      System.out.println("New");   
    }
 }
