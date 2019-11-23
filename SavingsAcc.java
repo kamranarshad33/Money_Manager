d7cc6e86c71b2c3f5bb35f0e75997d64443a22fb
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-11-22 23:52:23 -0500

    update (1.0) tracking withdrawals?

--- SavingsAcc.java
+++ SavingsAcc.java
@@ -1,4 +1,14 @@
 public class SavingsAcc extends Account{
     private final int monthlyWDLimit = 6;
+    // Track withdrawals?
+    // int numWithdrawals = 0;
+    
+    private SavingsAcc(){ 
+           
+    }
+    
+    public SavingsAcc(String username, double balance){
+        super(username, balance);
+    }
     
 }  
