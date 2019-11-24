cdbd08ae5eda674982239737b7977d1df8b840c2
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-11-24 17:20:19 -0500

    update (1.0): added transaction dates and category/type methods

--- Transaction.java
+++ Transaction.java
@@ -1,14 +1,15 @@
-//import java.util.Date;
+import java.util.Date;
 
 public abstract class Transaction{
     private double amount;
-    // private Date transDate; //date needed for search
-    //private String transType;
-
+    private String transDate; //date needed for search
+    private String transType;
+   
     
     public double getAmount(){
         return this.amount;    
     }
+    
     private void setAmount(double amount){
         if(amount >= 0){ 
             this.amount = amount;
@@ -16,5 +17,27 @@ public abstract class Transaction{
         else{
             throw new IllegalArgumentException("The transaction amount cannot be negative.");
         }    
+    }
+    
+    public String getDate() {
+      return this.transDate;
+    }
+    
+    public void setDate(Date userDate) {
+        // formats date to String = MM/DD/YY
+        transDate = String.format("%tD", userDate);
+    }
+    
+    public String getTransType() {
+      return transType;
+    }
+    
+    public void setTransType(String type) {
+      if (type.trim().equalsIgnoreCase("deposit") || type.trim().equalsIgnoreCase("withdrawal")) {
+         this.transType = type;
+      }
+      else {
+         throw new IllegalArgumentException("Error: Deposit and withdrawal are only options.");
+      }
     }
 }  
