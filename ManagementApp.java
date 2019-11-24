0d3b08743cfbd2bf99685133aab6169b6ed47da6
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-11-24 15:46:03 -0500

    Revert "update(1.0.2): added new/existing user paths"
    This reverts commit daec041c76fbbed19d56a5a20745d101982d4caa.

--- ManagementApp.java
+++ ManagementApp.java
@@ -9,18 +9,8 @@ public class ManagementApp{
             //addBankAccount(info);
             //validateAcc(acc)
         //do you have an account?
-      String start = "Welcome to the Money Manager! \n\n Do you have an existing account?";
-      
-      // Different methods based on existing or new account path?
-      //yes? || no?
-      //gather acc info || make account?
-      if (JOptionPane.showConfirmDialog(null, start) == JOptionPane.YES_OPTION) {
-         login(); // Existing user
-      }
-      else {
-         signup(); // New user
-      }        
-
+        //yes? || no?
+        //gather acc info || make account?
         //validate acc info || yes? / no?
         //valid? / not valid? || gather new acc info / quit
         //main menu || again? / signup? / quit? || new? / not new?
@@ -67,58 +57,6 @@ public class ManagementApp{
             }
         }while(menuOption != 5);
     }
-    
-   // Existing Account path
-   public static void login() { // validate + return validated Acct obj to main?
-      System.out.println("Existing"); // testcheck
-      // Gather User Info
-      String uname = "";
-      String pw = "";
-      boolean valid = false;
-      do {
-         uname = JOptionPane.showInputDialog(null, "Username: ", "Existing User Login", JOptionPane.QUESTION_MESSAGE);
-         pw = JOptionPane.showInputDialog(null, "Password: ", "Existing User Login", JOptionPane.QUESTION_MESSAGE);
-         /*
-            // Bank = placeholder for correct object
-            if (Bank.validateUser(uname, pw) {
-               valid = true;
-               JOptionPane.showMessageDialog(null, "Welcome back, " + uname + "!", "Existing User Login");
-            }
-            else {
-               if (JOptionPane.showConfirmDialog(null, "Would you like to try again? ", "Existing User Login", YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                  login();
-               }
-            }
-         */
-         //valid = true;
-      } while(!valid);
-      // Go to main menu
-   }
-   
-   // New Account path
-   public static void signup() {
-      System.out.println("New"); // testcheck
-      // Gather User Info   
-      String uname = "";
-      String pw = "";
-      if (JOptionPane.showConfirmDialog(null, "Would you like to create an account?") == JOptionPane.YES_OPTION) {
-         System.out.println("Creating Account"); // testcheck
-         uname = JOptionPane.showInputDialog(null, "Enter a username: ", "New User Signup", JOptionPane.QUESTION_MESSAGE);
-         pw = JOptionPane.showInputDialog(null, "Enter a password: ", "New User Signup", JOptionPane.QUESTION_MESSAGE);
-         /*
-         // Bank = placeholder for correct object
-         if (Bank.validateUser(uname, pw)) {
-            JOptionPane.showMessageDialog(null, "This account already exists. Taking to Existing User Login.", "New User Signup");
-            login();
-         }
-         else {
-            Bank.addUser(uname, pw) method implement?
-            JOptionPane.showMessageDialog(null, "Thank you! Welcome, " + uname + "!", "New User Signup");  
-         }
-         // Go to main menu
-         */
-      }
-   }
     
     public static boolean validateLogin(){
         return true;
